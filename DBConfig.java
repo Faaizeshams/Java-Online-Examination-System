39244f633938b14d9d956cbfe82c3d3eff78479a
Mohd Faaize Shams <142923967+Faaizeshams@users.noreply.github.com>
2025-11-16 17:32:37 +0530

    Create DBConfig.java

---
+++ DBConfig.java
@@ -0,0 +1,5 @@
+public class DBConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/examdb?useSSL=false&serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String PASSWORD = "password";
+}
